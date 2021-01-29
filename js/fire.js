24ef29885ef76c066cddef99e8563eaa8e63cfb1
wtdu <wtdu1992@gmail.com>
2021-01-29 18:48:29 +0800

    update

--- js/fire.js
+++ js/fire.js
@@ -12,7 +12,7 @@ var bigbooms = [];
 
 document.getElementById("iframMusic").onload = function(){
     var music = document.getElementById("music");
-    music.src = 'music.mp3';
+    music.src = 'https://flash-sgchat-cn.oss-cn-beijing.aliyuncs.com/do.mp3';
     music.oncanplay = function(){
         music.play();
     };
